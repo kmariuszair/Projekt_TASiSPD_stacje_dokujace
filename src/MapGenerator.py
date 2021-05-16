14a4215de513d5c09ad24a0378942482c2fa060b
kmariuszar <mariusz.kuchta@onet.com.pl>
2021-05-16 13:41:40 +0200

    Dalsza rozbudowa klas i funkcji

--- src/MapGenerator.py
+++ src/MapGenerator.py
@@ -2,30 +2,99 @@
 Generator mapy do znalezienia optymalnego rozmieszczenia stacji dokujących za pomocą tabu-search.
 """
 from typing import List
+import collections
+
+
 import RobotModel
 
 
 class TrafficMapGenerator:
 
-    def __init__(self, allowed_positions_map: np.array, robots_swarm: RobotsSwarm):
+    def __init__(self, allowed_positions_map: np.array, robots_swarm: RobotsSwarm, docking_stations_map: np.array):
         self.__allowed_positions = allowed_positions_map
         self.__robots_swarm = robots_swarm
+        # tam, gdzie wartości są większe od zera, tam znajdują się stacje dokujące, wartość danej komórki oznacza
+        # prędkość ładowania
+        self.__docking_stations_map = docking_stations_map
+        self.__paths_to_docks = {}
 
-    def generate_map(self):
+    def generate_map(self, sim_len: int):
         """
         Generacja mapy.
         Właściwie jest to symulacja działania robotów w magazynie/szklarni. Losowo wybierane są  obciążenia oraz
         kierunki ruchu robotów. Tutaj roboty są sterowane. Jeśli ich poziom baterii jest niski, to kierowane są do
         najbliższej stacji ładującej
+
+        :sim_len: długość symulacji w iteracjach
         """
-        pass
+        for _ in range(sim_len):  # powtarza kroki symulacji tak długo, jak zadano
+            for robot in self.__robots_swarm:  # aktualizacja stanu dla każdego robota
+                if robot.failure_detected():  # jeśli robot ma usterkę, to jest pomijany
+                    continue                  # (nie zakładamy możliwości naprawy w czasie symulacji)
+                elif robot.is_loading():
+                    r_pos = robot.get_actual_position()
+                    robot.make_move(np.array([0, 0]), 0, self.__docking_stations_map[r_pos[0]][r_pos[1]])
+                else:
+                    if robot.battery_low():  # jeśli robot ma mało baterii
+                        r_pos = robot.get_actual_position()
+                        if self.__docking_stations_map[r_pos[0]][r_pos[1]] > 0:
+                            robot.make_move(np.array([0, 0]), 0, self.__docking_stations_map[r_pos[0]][r_pos[1]])
+                            # usuwa tymczasowo wygenerowaną ścieżkę do najbliższej stacji dokującej
+                            # powinna zostać usunieta pusta lista
+                            del self.__paths_to_docks[robot.get_id()]
+                        else:
+                            direction = self.__goto_nearest_dock(robot.get_id(), robot.get_actual_position())
+                            robot.make_move(direction, 0, 0)
+                    else:  # jeśli robot jest sprawny
+                        direction = self.__generate_allowed_move(robot.get_actual_position())
+                        random_load = np.random.randi(-5, 5)
+                        robot.make_move(direction, random_load, 0)
+
+    def __generate_allowed_move(self, actual_position):
+        random_move = np.random.randint(-1,2, 2)
+        while self.__allowed_positions[actual_position[0]+random_move[0]][actual_position[1]+random_move[1]] != 0:
+            random_move = np.random.randint(-1, 2, 2)
+        return random_move
+
+    def __goto_nearest_dock(self, robot_id: int, robot_position: np.array):
+        if robot_id not in self.__paths_to_docks.keys():
+            self.__paths_to_docks[robot_id] = self.__bfs(robot_position)
+
+        path = self.__paths_to_docks[robot_id]
+        next_point = path.pop(0)
+        xdiff, ydiff = robot_position[0] - next_point[1], robot_position[1] - next_point[0]
+        return np.array([xdiff, ydiff])
+
+    def __bfs(self, start):
+        """
+        Znajdowanie ścieżki do najbliższej stacji ładującej. Zwraca listę krotek określających indeksy pozycji.
+        Trzeba uważać, bo zwraca indeksy w odwrotnej kolejnosci, niż przyjmuje je numpy.array.
+        Algorytm uwzględnia ograniczenia w postaci barier oraz istnienie wielu stacji ładujących.
+
+        :start: krotka zawierająca współrzędne punktu startowego (x0, y0)
+        """
+        grid = self.__allowed_positions
+        width = grid.shape[1]
+        height = grid.shape[0]
+        queue = collections.deque([[start]])
+        seen = set([start])
+        while queue:
+            path = queue.popleft()
+            x, y = path[-1]
+            if self.__docking_stations_map[y][x] == 1:
+                return path
+            for x2, y2 in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
+                if 0 <= x2 < width and 0 <= y2 < height and grid[y2][x2] != 1 and (x2, y2) not in seen:
+                    queue.append(path + [(x2, y2)])
+                    seen.add((x2, y2))
+        raise RuntimeError("Nie można odnaleźć najbliższej stacji dokującej")
 
 
 def generate_random_settings(settings_number: int, allowed_positions_map: np.array):
     """
     Generowanie losowych parametrów robotów
     """
-    for _ in range(settings_number):
+    for id in range(settings_number):
         battery_size = np.random.randi(100, 200)
         starting_battery_level = np.random.randi(100, battery_size)
         max_load = np.random.randi(5, 20)
@@ -34,7 +103,7 @@ def generate_random_settings(settings_number: int, allowed_positions_map: np.arr
             x0, y0 = np.random.randi(0, allowed_positions_map.shape[0]), np.random.randi(0,
                                                                                          allowed_positions_map.shape[1])
         starting_position = np.array([x0, y0])
-        yield RobotModel.RobotSettings(battery_size, starting_battery_level, max_load, starting_position)
+        yield RobotModel.RobotSettings(battery_size, starting_battery_level, max_load, starting_position, id)
 
 
 def generate_swarm(robots_number: int, allowed_positions_map: np.array,
@@ -57,6 +126,7 @@ class RobotsSwarm:
 
     def __init__(self, robots_number: int, allowed_positions_map: np.array):
         self.robots_list = generate_swarm(robots_number, allowed_positions_map, settings_list=predefined_settings)
+        self.iter_count = 0
 
     def __getitem__(self, item):
         """
@@ -64,3 +134,14 @@ class RobotsSwarm:
         """
         return self.robots_list[item]
 
+    def __iter__(self):
+        self.iter_count = 0
+        return self
+
+    def __next__(self):
+        if self.iter_count >= len(self.robots_list):
+            raise StopIteration
+        else:
+            robot = self.robots_list[self.iter_count]
+            self.iter_count += 1
+            return robot
--- src/RobotModel.py
+++ src/RobotModel.py
@@ -17,10 +17,21 @@ class Robot:
         :loading_speed: prędkość ładowania (wartość dodatnia jeśli robot się ładuje, zerowa jeśli nie)
         :nearest_docking_station: najbliższa stacja dokująca
         """
+        if self.__state.actual_load + new_load < 0:  # gdyby się zdarzyło, że chcielibyśmy wziąć z robota więcej niż
+            new_load = self.__state.actual_load      # ma załadowane
         self.__state.update_state(direction, new_load, loading_speed)
 
     def get_actual_position(self) -> np.array:
         return self.__state.actual_position
+
+    def battery_low(self) -> bool:
+        return self.__state.battery_low
+
+    def failure_detected(self) -> bool:
+        return self.__state.failure
+
+    def get_id(self) -> int:
+        return self.__settings.id
 
 
 class RobotState:
@@ -38,6 +49,7 @@ class RobotState:
         self.failure = False
         self.battery_low = False
         self.actual_position = starting_settings.starting_position
+        self.is_loading = False
 
     def update_state(self, direction: np.array, new_load: int, loading_speed: int):
         """
@@ -48,7 +60,12 @@ class RobotState:
             Robot w czasie normalnego działania
             """
             self.battery_level -= self.actual_load
-            self.battery_level += loading_speed
+            self.battery_level += loading_speed if self.battery_level + loading_speed < self.battery_size else \
+                self.battery_size - self.battery_level
+            if loading_speed > 0:
+                self.is_loading = True
+            else:
+                self.is_loading = False
 
             self.actual_load += new_load
 
@@ -70,8 +87,9 @@ class RobotState:
 
 class RobotSettings:
 
-    def __init__(self, battery_size, starting_battery_level, max_load, starting_position: np.array):
+    def __init__(self, battery_size, starting_battery_level, max_load, starting_position: np.array, id: int):
         self.battery_size = battery_size
         self.starting_battery_level = starting_battery_level
         self.max_load = max_load
         self.starting_position = starting_position
+        self.id = id
